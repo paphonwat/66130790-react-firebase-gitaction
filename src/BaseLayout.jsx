7af7d964a212d9b24ae8252b8e3a0726ac8c3134
Boss_paphw <paphonwat.w@gmail.com>
2024-04-20 16:01:52 +0700

    workflow added

--- src/BaseLayout.jsx
+++ src/BaseLayout.jsx
@@ -17,7 +17,7 @@ export default class BaseLayout extends Component {
               <Nav.Link href="/report">Home</Nav.Link>
               <Nav.Link href="/product">Products</Nav.Link>
             </Nav>
-            <div style={{ color: "white" }}>Siwakorn Banluesapy</div>
+            <div style={{ color: "white" }}>Paphonwat Wattanathatniroj</div>
           </Container>
         </Navbar>
         <p></p>
